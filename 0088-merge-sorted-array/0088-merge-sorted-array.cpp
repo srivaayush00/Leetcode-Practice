29999a9eca8ccf1eec53862baafa097364e568ff
Ayush Srivastava <ayush.sriva00@gmail.com>
2025-06-28 05:20:16 +0530

    Time: 0 ms (100%), Space: 12.4 MB (8.93%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,36 +1,20 @@
 class Solution {
 public:
-    void merge(vector<int>& a, int m, vector<int>& b, int n) {
-        vector<int> ans(n+m);
-        int i=0,j=0,p=0;
-        while(i!=m && j!=n){
-            
-            if(a[i]<b[j]){
-                // cout<<"Yess";
-                ans[p]=(a[i]);i++;p++;
+    void merge(vector<int>& nums1, int m, const vector<int>& nums2, int n) {
+        int p1 = m - 1;    // Pointer for end of nums1
+        int p2 = n - 1;    // Pointer for end of nums2
+        int p = m + n - 1; // Pointer for placement in nums1
+
+        // Merge from the back to avoid overwriting
+        while (p2 >= 0) {
+            if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+                nums1[p] = nums1[p1];
+                --p1;
+            } else {
+                nums1[p] = nums2[p2];
+                --p2;
             }
-            else {
-                ans[p]=b[j];
-                j++;p++;
-            }            
+            --p;
         }
-        for(;i<m;i++)
-        {
-            ans[p]=(a[i]);p++;
-        }
-        
-        for(;j<n;j++)
-        {
-            ans[p]=b[j];
-            p++;
-        }
-        
-        for(i=0;i<m+n;i++)
-            // cout<<ans[i];
-            a[i]=ans[i];
-            // ans.push_back(a[i]);
-        
-        // return ans;
-        
     }
 };
